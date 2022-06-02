38ba28c6263e97eb06dc0b3c1492dd82ac716e07
igor-stefan <igorstefan@aol.com>
2022-06-02 20:06:08 -0300

    adicionado pkg driver e inicio das aplicacoes com db

---
+++ internal/driver/driver.go
@@ -0,0 +1,61 @@
+package driver
+
+import (
+	"database/sql"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+// DB armazena as configurações de conexao com o database
+type DB struct {
+	SQL *sql.DB
+}
+
+var dbConn = &DB{}
+
+const maxOpenDbConn = 10
+const maxIdleDbConn = 5
+const maxDbLifetime = 5 * time.Minute
+
+// ConnectSQL cria uma estrutura de conexao para postgres
+func ConnectSQL(dsn string) (*DB, error) {
+	d, err := NewDatabase(dsn)
+	if err != nil {
+		panic(err)
+	}
+	d.SetMaxOpenConns(maxOpenDbConn)
+	d.SetMaxIdleConns(maxIdleDbConn)
+	d.SetConnMaxLifetime(maxDbLifetime)
+
+	dbConn.SQL = d
+
+	if err = testDB(d); err != nil {
+		return nil, err
+	}
+	return dbConn, nil
+}
+
+// testDB realiza um ping para o db conectado
+func testDB(d *sql.DB) error {
+	if err := d.Ping(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// NewDatabase inicia uma conexao com um novo database para a aplicação
+func NewDatabase(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
