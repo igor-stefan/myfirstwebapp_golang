ec0928950214d77d09511e18622478ece90a287a
igor-stefan <igorstefan@aol.com>
2022-06-04 18:55:56 -0300

    adicionado migration para fazer o seed em livros

---
+++ migrations/20220604215340_seed_livros_table.postgres.up.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.livros (nome_livro,num_emprestimos,created_at,updated_at) VALUES
+	 ('Uma Janela em Copacabana',0,'2022-06-04 10:34:04.539','2022-06-04 10:34:04.539'),
+	 ('Arsène Lupin: O Ladrão de Casaca',0,'2022-06-04 10:34:58.044','2022-06-04 10:34:58.044'),
+	 ('Arsène Lupin e a Agulha Oca',0,'2022-06-04 10:34:58.044','2022-06-04 10:34:58.044'),
+	 ('Arsène Lupin e os dentes do tigre',0,'2022-06-04 10:34:58.044','2022-06-04 10:34:58.044'),
+	 ('Arsène Lupin e o triângulo de ouro',0,'2022-06-04 10:34:58.044','2022-06-04 10:34:58.044'),
+	 ('Arséne Lupin Contra Herlock Sholmes',0,'2022-06-04 10:34:58.044','2022-06-04 10:34:58.044'),
+	 ('São Bernardo',0,'2022-06-04 10:34:58.044','2022-06-04 10:34:58.044');
