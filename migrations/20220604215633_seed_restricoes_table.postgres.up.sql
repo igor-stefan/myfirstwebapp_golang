93e78e6cf59e19246dff6706bd4267315c88586f
igor-stefan <igorstefan@aol.com>
2022-06-04 18:57:42 -0300

    adicionado migration para fazer o seed na tabela restricoes

---
+++ migrations/20220604215633_seed_restricoes_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restricoes (nome_restricao,created_at,updated_at) VALUES
+	 ('emprestado_usuario','2022-06-04 10:45:52.774','2022-06-04 10:45:52.774'),
+	 ('escolha_admin','2022-06-04 10:46:33.277','2022-06-04 10:46:33.277');
