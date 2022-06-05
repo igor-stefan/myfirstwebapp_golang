edd069438eb2ff238960c461cc08a4b59a6e5a9a
igor-stefan <igorstefan@aol.com>
2022-06-05 17:40:45 -0300

    ajustado datepicker

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -9,7 +9,6 @@ import (
 // DataBaseRepo é uma interface que permite que as funcionalidades armazenadas sejam utilizadas em outros pkgs (handlers, por exemplo)
 type DataBaseRepo interface {
 	AllUsers() bool
-
 	InsertReserva(res models.Reserva) (int, error)
 	InsertLivroRestricao(r models.LivroRestricao) error
 	SearchAvailabilityByDatesByRoomID(inicio, fim time.Time, livroID int) (bool, error)
