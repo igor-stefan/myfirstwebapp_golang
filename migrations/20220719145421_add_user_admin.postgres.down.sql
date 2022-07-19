7f3ebc34ab428ee6c59d641ec1906b47ea1cfff8
igor-stefan <igorstefan@aol.com>
2022-07-19 12:23:36 -0300

    adicionado novo template e novas migracoes

---
+++ migrations/20220719145421_add_user_admin.postgres.down.sql
@@ -0,0 +1,2 @@
+DELETE FROM public.users WHERE nome = 'Chop';
+DELETE FROM public.users WHERE nome = 'Ad';
---
+++ migrations/20220719145421_add_user_admin.postgres.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.users (nome,sobrenome, email, senha, acces_level, created_at,updated_at) VALUES
+	('Chop','Chop','chop@chop.com', '$2a$12$gPrjvWsthpRZg9tOLiDuJusdjGyKeKDeH3jzTxDdeQSQSdb6Qah/6', 5, '2022-06-04 10:34:04.539','2022-06-04 10:34:04.539'),
+	('Ad','Min','admin@admin.com', '$2a$12$5CRysGLIdlrwgx9zaGXwyO4XlvddaBisTgh/6atbA1fM3Xibf9L.C', 5, '2022-06-04 10:34:04.539','2022-06-04 10:34:04.539');
+
